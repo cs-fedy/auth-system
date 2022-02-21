42ac090a2dbfabde781446f8ac3872508017285a
cs-fedy <fedi.abd001@gmail.com>
2022-02-21 21:59:52 +0100

    docs removed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,6 @@
 import { CommonRouteConfig } from '@common'
 import AuthRoute from './auth.route'
-import DocsRoute from './docs.route'
 
-const routes: Array<CommonRouteConfig> = [
-  new DocsRoute('/docs'),
-  new AuthRoute('/auth'),
-]
+const routes: Array<CommonRouteConfig> = [new AuthRoute('/auth')]
 
 export default routes
